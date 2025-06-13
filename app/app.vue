13686ffef1ce800ea6e232921c9ab8f8fab3b228
nvxroads <nvxroads@gmail.com>
2025-06-13 17:27:43 +0300

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: 'NuxtHub Starter',
+  title: 'NuxtHub Starter - staging',
   description: 'A Nuxt template to build your full-stack application on the edge.'
 })
 </script>
